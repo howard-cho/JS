7b3cbdbaea26d2747031a6b9f49462399b275d59
howard-cho <40878835+howard-cho@users.noreply.github.com>
2018-11-05 18:19:44 -0700

    Copy files from Google Drive to GitHub

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// This is a comment
+const tables = 4 // 4 tables in the room
+console.log(44) // 44
+
+/* This is a comment */
+
+/* 
+    Everything
+    is
+    commented
+    in
+    this
+    example.
+*/
+
